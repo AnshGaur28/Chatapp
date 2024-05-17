eb32d78c30f34f30a8688bb7a1a420d04713ccbf
ANSH <ansh28.dinesh30@gmail.com>
2024-05-17 12:29:55 +0530

    minor change to Lnding Page

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -4,7 +4,9 @@ export default function LandingPage(){
     return (
         <div className='h-screen '>
             <div className='text-4xl font-semibold text-gray-600 italic flex justify-center items-center'>Chat Application</div>
-            <div className='flex justify-center  text-lg' ><button className='bg-green-600  rounded-lg hover:bg-green-400 p-2 m-2' onClick={()=>{navigate('/singleChat')}}>Ask a query</button></div>
+            {sessionStorage.getItem('jwt') && <div className='flex justify-center  text-lg' ><button className='bg-green-600  rounded-lg hover:bg-green-400 p-2 m-2' onClick={()=>{navigate('/singleChat')}}>Ask a query</button></div> }
+            {sessionStorage.getItem('jwt')==null && <div className='flex justify-center  text-lg' ><button className='bg-green-600  rounded-lg hover:bg-green-400 p-2 m-2' onClick={()=>{navigate('/Login')}}>Login</button></div> }
+            
         </div>
     )
 }
