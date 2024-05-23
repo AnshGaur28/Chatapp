8afbf8d88ad64159eeb1d7a268c30e22fd9fba41
ANSH <ansh28.dinesh30@gmail.com>
2024-05-23 14:43:39 +0530

    Added client side file upload and service

--- client/src/pages/singleChat.jsx
+++ client/src/pages/singleChat.jsx
@@ -2,19 +2,55 @@ import { useForm } from "react-hook-form";
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import { useNavigate } from "react-router-dom";
-import EmojiPicker from 'emoji-picker-react';
-
+import EmojiPicker from "emoji-picker-react";
 
 function SingleChat() {
   const chatContainerRef = useRef(null);
   const jwt = sessionStorage.getItem("token");
   const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState("");
-  const [emoji , setEmoji] = useState(false);
+  const [emoji, setEmoji] = useState(false);
+  const [media, setMedia] = useState(false);
+  const [file, setFile] = useState(null);
 
   const activityRef = useRef(null);
   const socketRef = useRef(null);
   const navigate = useNavigate();
+
+  const handleMediaPanel = async () => {
+    setMedia(!media);
+  };
+
+  const handleFileUpload = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const arrayBuffer = reader.result;
+        const buffer = new Uint8Array(arrayBuffer);
+        socketRef.current.emit(
+          "uploadFile",
+          {
+            file: buffer,
+            fileName: file.name,
+            fileType: file.type,
+            roomId: `room_${socketRef.current.id}`,
+            token: sessionStorage.getItem("token"),
+          },
+          (status) => {
+            console.log(status);
+          }
+        );
+      };
+      reader.readAsArrayBuffer(file);
+    } else {
+      console.log("No file selected");
+    }
+    setMedia(false);
+  };
   useEffect(() => {
     if (!socketRef.current) {
       const socket = io("ws://localhost:3000");
@@ -22,6 +58,12 @@ function SingleChat() {
       socket.emit("createRoom", sessionStorage.getItem("token"));
       socket.on("message", async (message) => {
         setMessages((messages) => [...messages, message]);
+      });
+
+      socketRef.current.on("fileUploaded", async (data) => {
+        const { fileName, fileType, file, username, role } = data;
+        console.log({ fileName, fileType, file, username, role });
+        setMessages((messages) => [...messages, data]);
       });
 
       socket.on("open", () => {
@@ -53,15 +95,15 @@ function SingleChat() {
     }
     setMessageInput("");
   };
-  
+
   const handleEmojiClick = (emojiData) => {
     setMessageInput(messageInput + emojiData.emoji);
   };
   const handleClose = async () => {
     console.log("Leaving Room");
     socketRef.current.emit("clientLeaveRoom");
-    sessionStorage.removeItem('token')
-    sessionStorage.removeItem('username')
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("username");
     navigate("/");
   };
   return (
@@ -90,40 +132,152 @@ function SingleChat() {
             ref={chatContainerRef}
           >
             {messages.map((message, index) => {
-              return (
-                <div className="scroller-item text-md" key={index}>
-                  {message && message.username && (
-                    <div className="my-2 mx-4">
+              if (message.content) {
+                return (
+                  <div className="scroller-item text-md" key={index}>
+                    {message && message.username && (
+                      <div className="my-2 mx-4">
+                        <div
+                          className={`flex ${
+                            message.role == "client"
+                              ? "justify-end"
+                              : "justify-start"
+                          }`}
+                        >
+                          <span
+                            className={`${
+                              message.role == "client"
+                                ? " row bg-[rgb(226,255,195)] "
+                                : "bg-gray-50 "
+                            } text-black  rounded-md p-2 `}
+                          >
+                            {/* {message.username} : {message.content} */}
+                            {message.content}
+
+                            <span className="text-xs text-gray-600 ml-5">
+                              {message.time}
+                            </span>
+                          </span>
+                        </div>
+                      </div>
+                    )}
+                    {message && message.username == null && (
+                      <div>{message.content}</div>
+                    )}
+                  </div>
+                );
+              } else {
+                const { fileType, fileName } = message;
+                const fileContent = `data:${fileType};base64,${message.file}`;
+
+                if (fileType.startsWith("image/")) {
+                  return (
+                    <div key={index} className=" flex ">
                       <div
-                        className={`flex ${
-                          message.role == "client"  ? "justify-end"
+                        className={`flex  w-full my-2  rounded-lg  p-2  ${
+                          message.role == "client"
+                            ? "justify-end"
                             : "justify-start"
                         }`}
                       >
-                        <span
-                          className={`${
-                            message.role == "client"
-                              ? " row bg-[rgb(226,255,195)] "
-                              : "bg-gray-50 "
-                          } text-black  rounded-md p-2 `}
+                        <div className="flex flex-col">
+                        <img
+                          src={`${fileContent}`}
+                          alt={fileName}
+                          className=" w-[200px] h-[200px] h-auto   bg-gray-100 rounded-lg p-2 "
+                        />
+                        <a
+                          href={`data:${fileType};base64,${message.file}`}
+                          download={fileName}
                         >
-                          {/* {message.username} : {message.content} */}
-                          {message.content}
-
-                          <span className="text-xs text-gray-600 ml-5">
-                            {message.time}
-                          </span>
-                        </span>
+                          {fileName}
+                        </a>
+                        </div>
                       </div>
                     </div>
-                  )}
-                  {message && message.username == null && (
-                    <div>{message.content}</div>
-                  )}
-                </div>
-              );
+                  );
+                } else if (fileType === "application/pdf") {
+                  const blob = new Blob([message.file], { type: fileType });
+                  const url = window.URL.createObjectURL(blob);
+                  return (
+                    <div key={index} className=" flex   ">
+                      <div
+                        key={index}
+                        className={`flex    my-2 w-full rounded-lg  p-2  ${
+                          message.role == "client"
+                            ? "justify-end"
+                            : "justify-start"
+                        }`}
+                      >
+                        <a href={url} download={fileName}>
+                          <div className="w-[100px] rounded-lg p-2 h-[100px] my-2 bg-gray-100 flex flex-col justify-center">
+                            <img
+                              alt={fileName}
+                              className="h-auto w-[50px] h-[50px]"
+                              src="/pdf.png"
+                            />
+                            {message.role}
+                            <p>{fileName}</p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  );
+                } else if (fileType.startsWith("text/")) {
+                  const blob = new Blob([message.file], { type: fileType });
+                  const url = window.URL.createObjectURL(blob);
+                  return (
+                    <div key={index} className=" flex ">
+                      <div
+                        key={index}
+                        className={`flex  flex-col my-2  rounded-lg  p-2  ${
+                          message.role == "client"
+                            ? "justify-end"
+                            : "justify-start"
+                        }`}
+                      >
+                        <a href={url} download={fileName}>
+                          <div
+                            className={`w-[100px]  rounded-lg p-2 h-[100px] my-2 bg-gray-100
+                  `}
+                          >
+                            <img
+                              alt={fileName}
+                              className=" h-auto w-[50px] h-[50px]"
+                              src="/txt-file.png"
+                            />
+                            <p className="w-full">{fileName}</p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return <p key={index}>Unsupported file type: {fileName}</p>;
+                }
+              }
             })}
-
+            {media && (
+              <div className="fixed flex flex-row w-full  bottom-20">
+                <div className="flex flex-col mb-2 border-2 p-2 border-gray-100 rounded-lg overflow-hidden">
+                  <input type="file" onChange={handleFileUpload} />
+                  <div className="flex flex-row space-x-5 justify-end">
+                    <button
+                      className="bg-gray-200 border-[1px] border-black text-black p-1 rounded-md m-2"
+                      onClick={handleUpload}
+                    >
+                      Upload
+                    </button>
+                    <button
+                      className="bg-gray-200 border-[1px] border-black text-black p-1 rounded-md m-2"
+                      onClick={() => setMedia(!media)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
             {emoji && (
               <div className="fixed flex flex-row w-full justify-start bottom-20">
                 <div className="flex flex-col mb-2 border-2 border-gray-100 rounded-lg overflow-hidden">
@@ -154,6 +308,15 @@ function SingleChat() {
               <img
                 className=" flex items-end justify-center mx-2 h-[30px] w-[30px]"
                 src="/happy.png"
+              />
+            </button>
+            <button
+              className="flex justify-center items-center "
+              onClick={handleMediaPanel}
+            >
+              <img
+                className=" flex items-end justify-center mx-2 h-[30px] w-[30px]"
+                src="/paper-pin.png"
               />
             </button>
             <form
